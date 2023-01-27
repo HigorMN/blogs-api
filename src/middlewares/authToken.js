cb227641431359d4cd35d3f68c412f55bc3aaaef
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-27 10:12:28 -0300

    feat: verificando token

---
+++ src/middlewares/authToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const userService = require('../services/user.service');
+
+const JWT_SECRET = process.env.JWT_SECRET || 'secret';
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ message: 'Token not found' });
+
+  try {
+    const verificationResponse = await jwt.verify(token, JWT_SECRET);
+    const user = await userService.findById(verificationResponse.id);
+
+    if (!user) return res.status(401).json({ message: 'Expired or invalid token' });
+
+    if (user.email !== verificationResponse.email) {
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
--- src/utils/JWT.js
+++ src/utils/JWT.js
@@ -5,24 +5,6 @@ const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 const generateToken = (login) =>
   jwt.sign({ login }, JWT_SECRET, { expiresIn: '5d' });
 
-const authToken = async (token) => {
-  if (!token) {
-    const error = new Error('missing auth token');
-    error.status = 401;
-    throw error;
-  }
-
-  try {
-    const verificationResponse = await jwt.verify(token, JWT_SECRET);
-    return verificationResponse;
-  } catch (err) {
-    const error = new Error('jwt malformed');
-    error.status = 401;
-    throw error;
-  }
-};
-
 module.exports = {
   generateToken,
-  authToken,
 };
