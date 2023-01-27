1a6c5feb084ea863cc802f9986e90be00e5e9273
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-27 18:32:15 -0300

    fix: removendo categoryIds

--- src/middlewares/validatePost.js
+++ src/middlewares/validatePost.js
@@ -1,14 +1,9 @@
 module.exports = (req, res, next) => {
-  const { title, content, categoryIds } = req.body;
+  const { title, content } = req.body;
 
   if (!title || !content) {
     return res.status(400)
       .json({ message: 'Some required fields are missing' });
-  }
-
-  if (!categoryIds) {
-    return res.status(400)
-      .json({ message: 'one or more "categoryIds" not found' });
   }
   
   next();
