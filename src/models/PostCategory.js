da68632cee1d248b8eaa0014ed8d2c387d7bf30f
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-27 14:36:47 -0300

    feat: modelo PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategoryTable = sequelize.define('PostCategory', {
+    postId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      primaryKey: true, 
+    },
+    categoryId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      primaryKey: true, 
+    },
+  }, {
+    tableName: 'blog_posts',
+    underscored: true,
+    timestamps: false
+  })
+
+  PostCategoryTable.associate = ({ BlogPost, Category}) => {
+    BlogPost.belongsToMany(Category, {
+      as: 'posts',
+      through: PostCategoryTable,
+      foreignKey: 'post_id',
+      otherKey: 'category_id',
+    })
+
+    Category.belongsToMany(BlogPost, {
+      as: 'categories',
+      through: PostCategoryTable,
+      foreignKey: 'category_id',
+      otherKey: 'post_id',
+    })
+  }
+
+  return PostCategoryTable;
+}
