110e80b0a0ef286c8e217141ae9236682af826a0
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-27 15:48:59 -0300

    feat: validando post

---
+++ src/middlewares/validatePost.js
@@ -0,0 +1,15 @@
+module.exports = (req, res, next) => {
+  const { title, content, categoryIds } = req.body;
+
+  if (!title || !content) {
+    return res.status(400)
+      .json({ message: 'Some required fields are missing' });
+  }
+
+  if (!categoryIds) {
+    return res.status(400)
+      .json({ message: 'one or more "categoryIds" not found' });
+  }
+  
+  next();
+};
